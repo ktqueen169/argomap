6606963384457e7a63539a3dc5dcad8e60bfdbe9
Andii <bowiekatherine@gmail.com>
2026-06-17 15:44:24 -0400

    updating some broken links

--- js/popups.js
+++ js/popups.js
@@ -14,15 +14,12 @@ function escapeAttr(value) {
 const VACANT_FLOOR_DESC =
 	"Please contact the owner of this building for more information";
 
-function popupDetails(item, fallback = {}, options = {}) {
+function popupDetails(item, fallback = {}) {
 	const isVacant = item?.vacant === true;
 	const image = isVacant
 		? "images/vacancy.png"
 		: (item.img ?? fallback.img ?? "");
-	const link =
-		isVacant || options.hideDetailsLink
-			? ""
-			: (item.link ?? fallback.link ?? "");
+	const link = isVacant ? "" : (item.link ?? fallback.link ?? "");
 	const owner = item.owner ?? fallback.owner;
 	const safeDesc = escapeHtml(isVacant ? VACANT_FLOOR_DESC : (item.desc ?? ""));
 	const safeAlt = escapeAttr(item.name || fallback.name || "Location image");
@@ -116,6 +113,6 @@ function makeDistrictPopup(district) {
   <div class="popup-body ${themeClass}">
    <div class="popup-title">${escapeHtml(district.label)}</div>
    <div class="popup-district-row"><div class="popup-cat popup-cat-district">District</div></div>
-   ${popupDetails(district, {}, { hideDetailsLink: true })}
+   ${popupDetails(district)}
   </div>`;
 }
