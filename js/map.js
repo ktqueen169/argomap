a5c9e4838bf56386b44eec9a49f0345fb713593c
Andii <bowiekatherine@gmail.com>
2026-09-05 13:06:30 -0400

    scrolljailbreak
    the new popup logic created another hostage situation for those pop ups, so I'm jailbreaking it

--- js/map.js
+++ js/map.js
@@ -7,7 +7,7 @@ const GATE_WIDTH = 3000,
 	GATE_OFFSET_Y = 0;
 const EDGE_POPUP_OPTIONS = {
 	autoPan: true,
-	keepInView: true,
+	keepInView: false,
 	autoPanPadding: L.point(140, 140),
 	autoPanPaddingTopLeft: L.point(140, 140),
 	autoPanPaddingBottomRight: L.point(140, 140),
